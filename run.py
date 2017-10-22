6ae08e71661c2df50b0b7e40a5154c4fe77c3208
Zax <zax.rosenberg@gmail.com>
2017-10-22 03:00:12 -0500

    Add Procfile for heroku

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
+import os
 from app import app
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
